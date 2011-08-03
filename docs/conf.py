9023e2563f8079ad0d98a075907c313e1f8c2c9e
David Marin <dave@yelp.com>
2011-08-02 17:14:23 -0700

    whoops, this is copyright Yelp, not me

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'doloop'
-copyright = u'2011, David Marin'
+copyright = u'2011, Yelp'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
