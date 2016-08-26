3b58b16610240dfde2659563d0cf72298ab64cbd
David Marin <dm@davidmarin.org>
2016-08-26 14:31:45 -0700

    update Copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'doloop'
-copyright = u'2011, Yelp'
+copyright = u'2011-2016, Yelp'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
